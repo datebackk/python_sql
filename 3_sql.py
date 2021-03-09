b05af258c44ee5aceb02fa3ae9ae021fc6daaa8e
datebackk <caskooss@gmail.com>
2021-03-09 16:46:28 +0300

    first commit

---
+++ 3_sql.py
@@ -0,0 +1,19 @@
+import sqlite3
+import psycopg2
+
+conn = psycopg2.connect(host='127.0.0.1', user='postgres', password='root', dbname='python_sql')
+
+cursor = conn.cursor()
+
+# 1
+cursor.execute("SELECT UPPER(surname) ||';'|| UPPER(name) ||';'|| stipend ||';'|| kurs ||';'|| UPPER(city) ||';'|| "
+               "birthday ||';'|| univ_id FROM student")
+
+print('1\n', cursor.fetchall())
+
+# 2
+cursor.execute("SELECT SUBSTR(NAME, 1, 1) || '.' || SURNAME ||' Место жительства-'|| CITY || '; родился-' || MONTH(birthday) FROM STUDENT LIMIT 1")
+print('2\n', cursor.fetchall())
+
+
+conn.close()
